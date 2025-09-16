bf20babefd3f4cf1399f1fc598ae0487b3e42f95
Your Name Elbek <elbek0575@gmail.com>
2025-09-16 21:18:49 +0500

    Координат берадиган бот

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
 # accounts/urls.py
 from django.urls import path
-from .views import (register_boss, forgot_boss_password_start, 
-                    forgot_boss_password_verify, boss_login
+from .views import boss_login
+from bots.suv_kerak_bot import (register_boss, forgot_boss_password_start, 
+                    forgot_boss_password_verify, aiogram_webhook_view
 )
-from bots.suv_kerak_bot import aiogram_webhook_view
 
 urlpatterns = [
     path("boss/register/", register_boss),                 # JSON/Form
