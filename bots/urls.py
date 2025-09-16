78bddf2ca6a2ba8f5004431603ee0dadfee121f6
Your Name Elbek <elbek0575@gmail.com>
2025-09-16 21:44:06 +0500

    Координат берадиган бот

--- bots/urls.py
+++ bots/urls.py
@@ -10,8 +10,8 @@ from .suv_kerak_bot import (
 app_name = "bots"
 
 urlpatterns = [
-    path("register/", register_boss, name="register_boss"),
-    path("forgot-password/start/",  forgot_boss_password_start,  name="forgot_pwd_start"),
-    path("forgot-password/verify/", forgot_boss_password_verify, name="forgot_pwd_verify"),
+    path("boss/register/", register_boss, name="register_boss"),
+    path("boss/forgot-password/start/",  forgot_boss_password_start,  name="forgot_pwd_start"),
+    path("boss/forgot-password/verify/", forgot_boss_password_verify, name="forgot_pwd_verify"),
     path("aiogram-bot-webhook/",   aiogram_webhook_view,         name="aiogram_webhook"),
 ]
